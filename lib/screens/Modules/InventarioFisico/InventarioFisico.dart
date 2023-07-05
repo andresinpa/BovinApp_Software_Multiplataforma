eeda4d48b128480170d53ff3899b3153ef6ec1ac
andresinpa <andresinfantepaez@gmail.com>
2023-07-05 00:04:41 -0500

    Diseño pantalla Inventario Fisico

--- lib/screens/Modules/InventarioFisico/InventarioFisico.dart
+++ lib/screens/Modules/InventarioFisico/InventarioFisico.dart
@@ -1,244 +1,43 @@
+import 'package:BovinApp/Design/BackgroundBottom.dart';
+import 'package:BovinApp/Screens/Modules/InventarioFisico/GridInvFisico.dart';
+import 'package:BovinApp/Widgets/BottomBar.dart';
+import 'package:BovinApp/Widgets/Export/Widgets.dart';
 import 'package:flutter/material.dart';
 
-class InventarioFisico extends StatelessWidget {
+class InventarioFisico extends StatefulWidget {
   const InventarioFisico({super.key});
-
   @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              'BovinApp',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            centerTitle: true,
-            actions: <Widget>[
-              IconButton(
-                icon: const Icon(Icons.exit_to_app_rounded),
-                onPressed: () {},
-              ),
-            ],
-          ),
-          body: const Padding(
-            padding: EdgeInsets.only(top: 5),
-            child: MatrizImagenes(),
-          ),
-        ),
-      ],
-    );
-  }
+  InventarioFisicoApp createState() => InventarioFisicoApp();
 }
 
-class MatrizImagenes extends StatelessWidget {
-  const MatrizImagenes({
-    Key? key,
-  }) : super(key: key);
+class InventarioFisicoApp extends State<InventarioFisico> {
+  int currentIndex = 1;
+  void onTabSelected(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 2,
-      children: <Widget>[
-        const Column(
-          children: <Widget>[
-            SizedBox(
-              height: 109,
-            ),
-            Row(
-              children: [
-                SizedBox(
-                  width: 56.5,
-                ),
-                Text(
-                  'Inventario ',
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-                ),
-              ],
-            ),
-          ],
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: const AppBarRetroceder(title: 'Inventario Fisíco'),
+      body: SingleChildScrollView(
+        child: BackgroundBotttom(
+          height: size.height * 0.935,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: size.height * 0.1),
+              const GridInvFisico(),
+            ],
+          ),
         ),
-        Column(
-          children: [
-            const SizedBox(
-              height: 80,
-            ),
-            Row(
-              children: [
-                const Text(
-                  'Fisíco',
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Image.asset(
-                  'assets/icon/icon.png',
-                  width: 90,
-                  height: 90,
-                ),
-              ],
-            ),
-          ],
-        ),
-        //////////////////////////////////////////////////////////////////////////////////////
-        Column(
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(5.0),
-              height: 150,
-              width: 150,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: GestureDetector(
-                onTap: () => Navigator.pushNamed(context, 'ConsultaAlimentos'),
-                child: Image.asset('assets/images/inventario/Inventario1.png',
-                    fit: BoxFit.cover),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 2),
-              child: Text(
-                'Alimentos',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-              ),
-            )
-          ],
-        ),
-        ///////////////////////////////////////////////////////////////////////////////////////
-        Column(
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(5.0),
-              height: 150,
-              width: 150,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: GestureDetector(
-                onTap: () =>
-                    Navigator.pushNamed(context, 'ConsultaMedicamentos'),
-                child: Image.asset('assets/images/inventario/Inventario2.png',
-                    fit: BoxFit.cover),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 2),
-              child: Text(
-                'Medicamentos',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-              ),
-            )
-          ],
-        ),
-        ///////////////////////////////////////////////////////////////////////////////////////
-        Column(
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(5.0),
-              height: 150,
-              width: 150,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: GestureDetector(
-                onTap: () => Navigator.pushNamed(context, 'ConsultaFerreteria'),
-                child: Image.asset('assets/images/inventario/Inventario3.png',
-                    fit: BoxFit.cover),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 2),
-              child: Text(
-                'Ferretería',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-              ),
-            )
-          ],
-        ),
-        ///////////////////////////////////////////////////////////////////////////////////////
-        Column(
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(5.0),
-              height: 150,
-              width: 150,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: GestureDetector(
-                onTap: () => Navigator.pushNamed(context, 'ConsultaMaquinaria'),
-                child: Image.asset('assets/images/inventario/Inventario4.png',
-                    fit: BoxFit.cover),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 2),
-              child: Text(
-                'Maquinaria',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-              ),
-            )
-          ],
-        ),
-        ///////////////////////////////////////////////////////////////////////////////////////
-        Column(
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(5.0),
-              height: 150,
-              width: 150,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: GestureDetector(
-                onTap: () => Navigator.pushNamed(context, 'ConsultaOtros'),
-                child: Image.asset('assets/images/inventario/Inventario5.png',
-                    fit: BoxFit.cover),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 2),
-              child: Text(
-                'Otros',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-              ),
-            )
-          ],
-        ),
-        ///////////////////////////////////////////////////////////////////////////////////////
-        Column(
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(5.0),
-              height: 150,
-              width: 150,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: GestureDetector(
-                onTap: () => Navigator.pushNamed(
-                    context, 'NuevoRegistroInventarioFisico'),
-                child: Image.asset('assets/images/inventario/Inventario6.png',
-                    fit: BoxFit.cover),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 2),
-              child: Text(
-                'Nuevo registro',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-              ),
-            )
-          ],
-        ),
-        //////////////////////////////////////////////////////////////////////////////////////
-      ],
+      ),
+      bottomNavigationBar:
+          BottomBar(initialIndex: currentIndex, onTabSelected: onTabSelected),
     );
   }
 }
