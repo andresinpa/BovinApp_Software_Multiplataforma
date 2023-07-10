45ab8a5486e6e571c84b219de0556ea10fb4695c
andresinpa <andresinfantepaez@gmail.com>
2023-07-10 12:12:28 -0500

    Agregando Provider para controlar la persistencia de datos entre pantallas.

---
+++ lib/DTO/Services/UserProvider.dart
@@ -0,0 +1,13 @@
+import 'package:BovinApp/DTO/User.dart';
+import 'package:flutter/material.dart';
+
+class UserProvider with ChangeNotifier {
+  User _user = User();
+
+  User get user => _user;
+
+  set user(User newUser) {
+    _user = newUser;
+    notifyListeners();
+  }
+}
